fd4db8b78a22206dc09bd69156cb6e13ca2dabd9
Delirius - Daniel Herren <github@delirius.ch>
2023-03-17 00:41:56 +0100

    update explain

--- contao/languages/de/explain.php
+++ contao/languages/de/explain.php
@@ -4,7 +4,7 @@ declare (strict_types = 1);
 // contao/languages/en/explain.php
 $GLOBALS['TL_LANG']['XPL']['htmltag'] = [
 	['colspan', '<h3>Sicherheit</h3>
-Um die Sicherheit zu gewährleisten, werden bei der <strong>Frontend-Ausgabe</strong> die HTML-Tags <strong>gefiltert</strong>, gemäss den Contao-Einstellungen für erlaubte HTML-Tags und erlaubte HTML-Attribute. Wenn nötig, müssen spezielle HTML-Tags und HTML-Attribute in den Einstellungen -> Sicherheitseinstellungen hinzugefügt werden.<br><br>Beispiele:'],
+Um die Sicherheit zu gewährleisten, werden bei der <strong>Frontend-Ausgabe</strong> die HTML-Tags <strong>gefiltert</strong>, gemäss den Contao-Einstellungen für erlaubte HTML-Tags und erlaubte HTML-Attribute. Wenn nötig, müssen spezielle HTML-Tags und HTML-Attribute in den Einstellungen -> Sicherheitseinstellungen hinzugefügt werden.<br><br>Beispiele:', ],
 	['Erlaubte HTML-Tags:', '&lt;iframe&gt;'],
 	['Erlaubte HTML-Attribute:', 'iframe | src,style,allowfullscreen <br>button | type,disabled <br>a | type,role'],
 ];
--- contao/languages/en/explain.php
+++ contao/languages/en/explain.php
@@ -5,7 +5,7 @@ declare (strict_types = 1);
 $GLOBALS['TL_LANG']['XPL']['htmltag'] = [
 	['colspan', '<h3>Security</h3>
 To ensure security, HTML tags are <strong>filtered</strong> in the <strong>frontend output</strong> according to the Contao settings for allowed HTML tags and allowed HTML attributes. If necessary, special HTML tags and HTML attributes must be added in the Settings -> Security settings.<br><br>
-Examples:'],
+Examples:', ],
 	['Allowed HTML tags:', '&lt;iframe&gt;'],
 	['Allowed HTML attributes:', 'iframe | src,style,allowfullscreen <br>button | type,disabled <br>a | type,role'],
 ];
