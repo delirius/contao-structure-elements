d74981532961e621f908940277254525930f319d
Delirius - Daniel Herren <github@delirius.ch>
2023-03-22 00:43:41 +0100

    Update explain.php

--- contao/languages/en/explain.php
+++ contao/languages/en/explain.php
@@ -4,10 +4,10 @@ declare (strict_types = 1);
 // contao/languages/en/explain.php
 $GLOBALS['TL_LANG']['XPL']['htmltag'] = [
 	['colspan', '<h3>HTML-Tags</h3>
-HTML elements such as div, span, button, a, etc. HTML tags are allowed according to the Contao settings.'],
+HTML elements such as div, span, button, a, etc. HTML tags are allowed according to the Contao settings.', ],
 	['colspan', '<h3>Security</h3>
 To ensure security, HTML tags are <strong>filtered</strong> in the <strong>frontend output</strong> according to the Contao settings for allowed HTML tags and allowed HTML attributes. If necessary, special HTML tags and HTML attributes must be added in the Settings -> Security settings.<br><br>
-Examples:'],
+Example configuration:', ],
 	['Allowed HTML tags:', '&lt;iframe&gt;'],
 	['Allowed HTML attributes:', 'iframe | src,style,allowfullscreen <br>button | type,disabled <br>a | type,role'],
 ];
